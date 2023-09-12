d2689523b1793c60a89d34b2c4dd53dfbc74f2a4
Marc Sorrentino <mas2565@columbia.edu>
2023-09-12 19:07:12 -0400

    starter basicbehav v1

---
+++ speedPath.m
@@ -0,0 +1,12 @@
+function speedPath()
+    addpath('/home/electro/.speedgoat');
+    addpath(genpath('/home/electro/.speedgoat'));
+    addpath('/home/electro/.qnx');
+    addpath(genpath('/home/electro/.qnx'));
+    % addpath('/usr/local/MATLAB/R2023a/toolbox/slrealtime/target');
+    % addpath(genpath('/usr/local/MATLAB/R2023a/toolbox/slrealtime/target'));
+    % set_param('basicBehavior','SimUserIncludeDirs','/usr/local/MATLAB/R2023a/toolbox/slrealtime/target/kernel/dist/include');
+    % set_param('basicBehavior','SimUserIncludeDirs','"/usr/local/MATLAB/R2023a/toolbox/slrealtime/target/kernel/dist/include/verify"');
+    % set_param('basicBehavior','SimulationCommand','Update')
+    % set_param('basicBehavior','Include')    
+end
