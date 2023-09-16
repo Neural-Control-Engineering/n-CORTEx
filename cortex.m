1b7c8a63aac46ee7db6dd0eca990599cd6b359ef
Marc Sorrentino <mas2565@columbia.edu>
2023-09-16 17:09:17 -0400

    Update cortex.m

--- cortex.m
+++ cortex.m
@@ -1,7 +1,7 @@
 function cortex
-    params = setSlParams(struct);
+    params = setBaseParams(struct);
     warning('off', 'MATLAB:hg:AutoSoftwareOpenGL');
-    addpath(params.paths.nCORTEx_repo);
-    addpath(genpath(params.paths.nCORTEx_repo));
+    % addpath(params.paths.nCORTEx_repo);
+    % addpath(genpath(params.paths.nCORTEx_repo));
     nCORTEx_console;
 end
