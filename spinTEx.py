44605c51f1e45e053ae17d46a357c95e0debe00d
Marc Sorrentino <mas2565@columbia.edu>
2023-09-30 11:04:42 -0400

    Proper Acquisition

--- spinTEx.py
+++ spinTEx.py
@@ -5,8 +5,13 @@ from simple_pyspin import Camera, list_cameras
 from PIL import Image
 import cv2
 import signal
+import multiprocessing
+from multiprocessing import Process
 
 # MINI LIBRARY BUILT FROM SPINVIEW API (SPINNAKER SDK) TO CONFIGURE AND START A SPINVIEW ACQUISITION
+
+BUFFERSIZE=1000
+COMPRESSIONLEVEL=6
 
 def main():
     # Check if the required argument is provided
@@ -27,22 +32,14 @@ def main():
     pupilSN=spinParams["pupilSN"]
     whiskSN=spinParams["whiskSN"]
 
-    if execStatus=="start":
-
-        # Store the PID in an environment variable
-        # os.environ["startPID"] = str(os.getpid())
+    if execStatus=="start":                
+        frameBuffer=multiprocessing.Manager().list()
+        print('frameBuffer initiated')
 
         cameras = list_cameras()
         numCameras = cameras.GetSize()
         print(str(numCameras) + ' camera(s) found')
-
-        # Find pupil and whisker cameras if connected
-        # for camera in cameras:            
-        #     SN = camera.GetUniqueID()
-        #     SN = convertHex2Dec()
-            # cam.init()
-            # serialNum = cam.get_info('DeviceSerialNumber')['value']
-            # print('SN'+str(i)+': '+str(serialNum))
+       
         if camSelect==pupilSN:
             cam = Camera(pupilSN)
             # temporary: reset camera on startup
@@ -51,8 +48,8 @@ def main():
             cam.init()
             cam = setSpinParams(cam, spinParams['pupilCam']) # find and store pupil camera
             acqDir = os.path.join(saveDir,"Raw Pupil Data")
-            os.environ["pupilAcqPID"] = str(os.getpid())
-            print("pupilPID: ",os.getpid())
+            isKillVar='ISPUPKILL'            
+           
         elif camSelect==whiskSN:
             cam = Camera(whiskSN)
             # temporary: reset camera on startup
@@ -61,33 +58,44 @@ def main():
             cam.init()
             cam = setSpinParams(cam, spinParams['whiskCam'])
             acqDir = os.path.join(saveDir,"Raw Whisker Data")
-            os.environ["whiskAcqPID"] = str(os.getpid())
-            print("whiskPID: ",os.getpid())
+            isKillVar='ISWSKKILL'
+           
+        os.environ[isKillVar]='0' # init to 0 and check if set to 1
         
+        # Init directory to store frames
         if not os.path.exists(acqDir):
             os.mkdir(acqDir)        
 
+        # Start concurrent frame saving process
         print('starting acquisition')
-
-        # open Video Writers       
-        cam.start()       
-        i=0
+        endEvent=multiprocessing.Event()
+        print('starting saving thread')
+        saveProc = Process(target=saveFrames,args=(frameBuffer,acqDir, endEvent))        
+        saveProc.start()
+        # Begin Acquisition
+        cam.start() 
+        print('FPS: ',cam.AcquisitionResultingFrameRate)      
+        # i=0
         while True: # continue triggered acquisition until 'stop' is called        
             # print('camStarted')
             frame = cam.get_array()
-            cv2.imwrite(os.path.join(acqDir,"frame_"+str(i)+".png"),frame)
-            # Image.fromarray(frame).save(os.path.join(acqDir,"frame_"+str(i)+".png"))            
-            i+=1        
+            print('frame acquired')
+            frameBuffer.append(frame)
+            print('BUFFERLEN: ', len(frameBuffer))
+
+            if len(frameBuffer)>BUFFERSIZE:
+                while len(frameBuffer)>BUFFERSIZE:
+                    frameBuffer.pop(0)
+
+            if os.environ.get(isKillVar)=='1':
+                cam.stop()
+                endEvent.set()
+                print("Acquisition Complete, releasing resources")
+                break           
 
     elif execStatus=="stop":
-        pupPID = os.environ.get("pupilAcqPID")         
-        wskPID = os.environ.get("whiskAcqPID")        
-        print("killing pupil PID", pupPID)                        
-        os.kill(pupPID, signal.SIGTERM)        
-        print("killing whisker PID", wskPID)                        
-        os.kill(wskPID, signal.SIGTERM)
-        print("exiting ...")
-        sys.exit(1)
+        os.environ['ISPUPKILL']='1'
+        os.environ['ISWSKKILL']='1'        
     else:
         raise Exception("Missing ExecStatus!")  
 
@@ -106,6 +114,18 @@ def setSpinParams(camera, stgsDict):
         print('set '+str(key)+' to '+str(value))
 
     return camera
+
+def saveFrames(frameBuffer, acqDir, endEvent):
+    i=0
+    while True:
+        if len(frameBuffer)>0:
+            frame = frameBuffer.pop(0)
+            cv2.imwrite(os.path.join(acqDir, "frame_"+str(i)+".png"), frame, [cv2.IMWRITE_PNG_COMPRESSION, COMPRESSIONLEVEL])
+            i+=1 
+            print('frame_',i,' saved')
+            print('BUFFERLEN: ', len(frameBuffer))
+        elif endEvent.is_set():
+            break
 
 
 if __name__ == "__main__":
