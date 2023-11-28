bdfcf31940c2ef10bf91e647f60ff41ab4f9c4af
Marc Sorrentino <mas2565@columbia.edu>
2023-11-28 09:05:31 -0500

    ATTN working for dry run, added sample logsout to develop ssession analysis functon

--- JOLT_slrealtime_rtw/rtGetInf.cpp
+++ JOLT_slrealtime_rtw/rtGetInf.cpp
@@ -9,7 +9,7 @@
  *
  * Model version              : 1.352
  * Simulink Coder version : 9.9 (R2023a) 19-Nov-2022
- * C++ source code generated on : Tue Nov 21 14:43:03 2023
+ * C++ source code generated on : Wed Nov 22 10:27:36 2023
  *
  * Target selection: slrealtime.tlc
  * Note: GRT includes extra infrastructure and instrumentation for prototyping
