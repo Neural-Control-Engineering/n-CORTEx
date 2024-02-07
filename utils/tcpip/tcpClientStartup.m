ac13b8c63cf97dcddefee99e0854b17629a714d1
NEC Lab Shared Resources <106262764+NecLabResources@users.noreply.github.com>
2024-02-07 11:59:25 -0500

    tcp refactor

--- utils/tcpip/tcpClientStartup.m
+++ utils/tcpip/tcpClientStartup.m
@@ -1,5 +1,5 @@
 address = '128.59.87.69';
-model = 'SSpNeuromod_SW';
+model = 'SSpNeuromod';
 load_system(model);
 mdlWks = get_param(model,'ModelWorkspace');
 client = tcpclient(address,5000,"Timeout",5)
