3ee5a05e630d4e796b661dc04ebc7f74d9c91281
NEC Lab Shared Resources <106262764+NecLabResources@users.noreply.github.com>
2024-02-08 09:44:19 -0500

    clientStart

--- utils/tcpip/tcpClientStartup.m
+++ utils/tcpip/tcpClientStartup.m
@@ -1,15 +1,13 @@
 
-address = '128.59.87.69';
-model = 'SSpNeuromod';
-load_system(model);
-mdlWks = get_param(model,'ModelWorkspace');
-
-address = '169.254.103.10';
+% address = '128.59.87.69';
+% model = 'SSpNeuromod';
+% load_system(model);
+% mdlWks = get_param(model,'ModelWorkspace');
 % model = 'SSpNeuromod_SW';
 % load_system(model);
 % mdlWks = get_param(model,'ModelWorkspace');
-
-client = tcpclient(address,5000,"Timeout",5)
+address = '169.254.103.10';
+client = tcpclient(address,8001,"Timeout",5)
 % writeline(client,"donothing")
-assignin(mdlWks,'client',client)
-save_system(model);
+% assignin(mdlWks,'client',client)
+% save_system(model);
