d8180a6b3c6b27d1d771781f6da4d8f60733424a
NEC Lab Shared Resources <106262764+NecLabResources@users.noreply.github.com>
2024-02-20 18:33:03 -0500

    data RX

--- utils/readDataFcn_photon.m
+++ utils/readDataFcn_photon.m
@@ -2,10 +2,12 @@ function readDataFcn_photon(sg, pl, ~)
     disp("PV command received from Speedgoat.")
     PVcmd_vector = read(sg,sg.NumBytesAvailable,"uint8");   
     dosomething = (PVcmd_vector(1) == 1);   
-    switch dosomething
+    switch dosomething 
         case dosomething
-             pl.GetState("dwellTime")
-             sg.write("done")
+             % pl.GetState("dwellTime")
+             isDone = pl.SendScriptCommands("-ts");
+             write(sg,uint8(isDone));
+             % sg.write(isDone);
         otherwise
             disp("otherwise");
     end
