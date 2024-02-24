ae767497262d73e9ddb866ffb94a44f7b8057f77
Marc Sorrentino <mas2565@columbia.edu>
2024-02-24 02:23:25 -0500

    extractionPipeline plus acq2ext bridge

--- Extraction/Logging/updateExtractionLog.m
+++ Extraction/Logging/updateExtractionLog.m
@@ -1,5 +1,11 @@
-function updateExtractionLog(params, modality, sessionName)
+function extractionLog = updateExtractionLog(extractionLog, sessionLabel, ColName, Value, initVal)
     % given path to extraction log, as determined by projectDir and
     % modality, write a new session into the extraction log, initialized as
     % 0
+    if ~ismember(extractionLog,ColName)
+        extractionLog.(ColName) = repelem(initVal,height(extractionLog))';
+    else        
+    end
+    extractionLog(contains(extractionLog.SessionName,sessionLabel),:).(ColName)=Value;  
+    
 end
