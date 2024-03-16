6c856a3f2feac6c7aa2306fd95a6daaf5607eec4
C. L. Slater <50306851+codylslater@users.noreply.github.com>
2024-03-16 17:16:14 -0400

    npxls server active

--- Setup/Server/server_npxls.m
+++ Setup/Server/server_npxls.m
@@ -2,10 +2,10 @@ function npx = server_npxls(params)
     hostName = params.hostName;
     switch hostName
         case 'DESKTOP-MRI7THQ'
-            address = "128.150.49.93";
+            address = '128.59.150.93';
             modality = 'npxls';
     end
-    pl = actxserver('PrairieLink64.Application');
-    pl.Connect();
+    % pl = actxserver('PrairieLink64.Application');
+    npx = SpikeGL(address);   
 
 end
