b3c2d012fb83b4a753cf60bb7525dbb410582c90
C. L. Slater <50306851+codylslater@users.noreply.github.com>
2024-03-26 19:34:47 -0400

    extraction switches

--- Extraction/Extraction-Modules/extraction.m
+++ Extraction/Extraction-Modules/extraction.m
@@ -3,21 +3,29 @@ function extraction(params)
     % TIER 1 : RAW
     % run basic extraction for raw data from each modality acquired during
     % experimentation    
-    extractRAW(params);
+    if strcmp(params.extrctItms.RAW.RAWSwitch.Value,'On')
+        extractRAW(params);
+    end
     
 
     % TIER 2 : EXTRACTED
     % experiment specific, trial-wise pre-processing of data modalities    
-    extractEXT(params);
+    if strcmp(params.extrctItms.EXT.EXTSwitch.Value,'On')
+        extractEXT(params);
+    end
 
 
     % TIER 3 : DATASTORE
     % user-generated datastore prepped for analysis    
-    extractDTS(params);
+    if strcmp(params.extrctItms.DTS.DTSSwitch.Value,'On')
+        extractDTS(params);
+    end
     
 
     % TIER 4 : FEATURE / VECTOR SET
     % feature array for classification and machine learning
-    extractFTR(params);
+    if strcmp(params.extrctItms.FTR.FTRSwitch.Value,'On')
+        extractFTR(params);
+    end
 
 end
