1dcb847d68d27ce40932791707a1e779ed085da0
C. L. Slater <50306851+codylslater@users.noreply.github.com>
2024-04-02 09:08:22 -0400

    npxls channel mapping

--- Visualization/Npxls/mapChan2Npxls.m
+++ Visualization/Npxls/mapChan2Npxls.m
@@ -1,4 +1,15 @@
 function NT = mapChan2Npxls(regMap, dataStream)
     % bin 384 channel datastream into npxls style heatmap
     % return a neuropixels tensor (NT) for video gen
+    NT = zeros(96,4,size(dataStream,2));
+    for i = 1:height(regMap)
+        npxChan = regMap(i,:);
+        channelNum = npxChan.channel{1};
+        X = npxChan.X{1};
+        Y = npxChan.Y{1};
+        X_shift = (X-11)/16+1;
+        Y_shift = floor((Y-240) / 40)+1;
+        NT(Y_shift,X_shift,:) = dataStream(i,:);
+    end
+
 end
--- Visualization/plot_regionXpowerbands.m
+++ Visualization/plot_regionXpowerbands.m
@@ -13,20 +13,52 @@ function plot_regionXpowerbands(regMap, bands, lfp)
         bRange = bands.(band);
         bandPwr = psd(:,bRange(1):bRange(2),:);
         bandPwr = mean(bandPwr,2);
-        % iterate through channel-region map
-        figure
-        hold on;
+        % iterate through channel-region map                        
+        regAvgBuffer = [];
+        Colors = [];
+        Labels = [];
+        figure        
+        sh = stackedplot([]);
+        sh.Title=sprintf("%s power %d-%d Hz",band,bRange(1),bRange(2));
+        sh.Visible="off";
+        sh.XData = t_stimAlgn';
+        m=1;
         for i = 1:height(regMap)
             npxChan = regMap(i,:);
             channelNum = npxChan.channel{1};
             region = npxChan.region{1};
-            shortRegion = split(region,'-');
-            shortRegion = shortRegion{1};
+            % shortRegion = split(region,'-');
+            % shortRegion = shortRegion{1};
             color = npxChan.color{1};
-            
-            chanTC = bandPwr(channelNum,:);
-            plot(chanTC, 'Color',sprintf("#%s",color))
+            if i > 1
+                npxChanPrev = regMap(i-1,:);
+                regionPrev = npxChanPrev.region{1};
+                colorPrev = npxChanPrev.color{1};
+                if ~strcmp(region, regionPrev) || (i == height(regMap))
+                    regAvg = mean(regAvgBuffer,1);
+                    sh.YData = [sh.YData, regAvg'];                
+                    Colors = [Colors; sprintf("#%s",colorPrev)];
+                    Labels = [Labels; string(regionPrev)];
+                    m=m+1;
+                    regAvgBuffer = [];                
+                else
+                    regAvgBuffer = [regAvgBuffer; bandPwr(channelNum,:)];
+                end 
+            else
+                regAvgBuffer = [regAvgBuffer; bandPwr(channelNum,:)];                 
+            end
         end
-        hold off;
+
+        for i = 1:size(Colors,1)
+            sh.LineProperties(i).Color = Colors(i);
+        end
+        % sh.LineProperties(m).Color = sprintf("#%s",color);
+        sh.DisplayLabels = Labels;
+        sh.XLabel = "time (s)";
+        sh.Visible="on";   
+        set(gcf,'Position',[452,-573,560,1222]);
+        set(gcf,'Color',[1.00,1.00,1.00]);
+        saveas(gcf,sprintf("%s.png",band))
+        saveas(gcf,sprintf("%s.fig",band))
     end
 end
