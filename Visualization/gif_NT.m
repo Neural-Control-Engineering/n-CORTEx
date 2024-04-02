e729e6767ee1e892ea244b47f6cca4a9debe65d2
C. L. Slater <50306851+codylslater@users.noreply.github.com>
2024-04-02 13:52:34 -0400

    stim single channel test

---
+++ Visualization/gif_NT.m
@@ -0,0 +1,21 @@
+function gif_NT(NT, t, t_range, fileName)    
+    
+    t_cond = t > t_range(1) & t < t_range(2);    
+
+    % plot Gif over axis  
+
+    NTaxs.t = t(t_cond);    
+    NTaxs.chan = 1:96;
+    NTaxs.sweepAx='t';
+
+    NTData.dataStream = NT(:,:,t_cond);
+    NTData.axs = NTaxs;
+    NTData.plot='NT';
+    NTData.label='sec';
+    NTData.sweepDim = 3;
+
+    gifCfg.filename='NT_6736_t7_3_11.gif';
+    % gifCfg.filename=sprintf("NT_%s.gif");
+    gifCfg.frameDelay = 0.01;
+    gifOverAxis(gifCfg, {NTData});
+end
