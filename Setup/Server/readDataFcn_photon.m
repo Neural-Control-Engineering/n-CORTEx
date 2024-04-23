9c0ed41dcf1219220fe2a14bc295a465ad20e6aa
NEC Lab Shared Resources <106262764+NecLabResources@users.noreply.github.com>
2024-04-23 18:12:32 -0400

    new prairielink test commands

--- Setup/Server/readDataFcn_photon.m
+++ Setup/Server/readDataFcn_photon.m
@@ -11,20 +11,25 @@ function readDataFcn_photon(params, sgSrv, modSrv)
     %     otherwise
     %         disp("otherwise");
     % end
-    cmdBuffer = find(PVcmd_vector==1);
-    % UNCOMMENT
-    % for i = 1:length(cmdBuffer)
-    %     switch cmdBuffer(i)
-    %         case 1 % GetImage | -gi
-    %             scriptCmd = sprintf("-SetFileIteration %d",10);
-    %             modSrv.SendScriptCommands(scriptCmd);
-    %             modSrv.GetImage(1);
-    %         case 2 % TSeries | -ts
-    %         case 3 % DEBUG
-    %             scriptCmd = sprintf("-Get");
-    %             modSrv.SendScriptCommands(scriptCmd);
-    % 
-    %     end
-    % end
+    cmdBuffer = find(PVcmd_vector==1);    
+    for i = 1:length(cmdBuffer)
+        switch cmdBuffer(i)
+            case 1 % GetImage | -gi
+                scriptCmd = sprintf("-SetFileIteration %d",10);
+                modSrv.SendScriptCommands(scriptCmd);
+                modSrv.GetImage(1);
+            case 2 % TSeries | -ts    
+                
+            case 3 % Open Shutter
+                modSrv.SendScriptCommands("-SetHardShutter Open")
+            case 4 % Close Shutter
+                modSrv.SendScriptCommands("-SetHardShutter Close")
+            case 7 % DEBUG
+                scriptCmd = sprintf("-Get");
+                modSrv.SendScriptCommands(scriptCmd);
+                modSrv.SendScriptCommands("-gts");
+
+        end
+    end
 
 end
