1cdc489751c8bb0bddf90a5fe11225afc00eca59
C. L. Slater <50306851+codylslater@users.noreply.github.com>
2024-04-25 11:48:36 -0400

    updated extraction skeleton

--- Extraction/Extraction-Modules/SLRT/extractEXT_SLRT.m
+++ Extraction/Extraction-Modules/SLRT/extractEXT_SLRT.m
@@ -3,5 +3,15 @@ function extractEXT_SLRT(params, sessionsToExtract, Q)
     [sessionsLeftToExtract, extractionLog] = checkExtractionProgress(extractionLog,sessionsToExtract,'Extract_SLRT');
     sessions = sessionsToExtract.sessions;
     expmntExtractionHndl = str2func(sprintf("extract%s",params.extractCfg.experiment));
-    expmntData = expmntExtractionHndl(params, sessionsToExtract, Q);    
+    expmntData = expmntExtractionHndl(params, sessionsToExtract, Q);
+    % Load/Extract Data Modalities (using timing segmentation from SLRT)
+    extMods = [];
+
+    lfp = loadEXT_LFP(slrt.segTimes);
+    lfpExt = extractEXT_LFP(lfp);
+
+    ap = loadEXT_AP(slrt.segTimes);
+    apExt = extractEXT_AP(ap);
+
+    % Write Extracted Modalities
 end
