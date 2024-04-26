a1e7ac5303321abec36f53524a4c2a1aa4a13839
NEC Lab Shared Resources <106262764+NecLabResources@users.noreply.github.com>
2024-04-26 17:40:55 -0400

    two photon PVrx for timing cmds

--- Setup/Server/readDataFcn_photon.m
+++ Setup/Server/readDataFcn_photon.m
@@ -1,6 +1,7 @@
 function readDataFcn_photon(params, sgSrv, modSrv)
     disp("PV command received from Speedgoat.")
-    PVcmd_vector = read(sgSrv,sgSrv.NumBytesAvailable,"uint8");   
+    PVcmd = read(sgSrv,sgSrv.NumBytesAvailable,"uint8"); 
+    PVrx = zeros(25,1);
     % dosomething = (PVcmd_vector(1) == 1);   
     % switch dosomething 
     %     case dosomething
@@ -12,24 +13,28 @@ function readDataFcn_photon(params, sgSrv, modSrv)
     %         disp("otherwise");
     % end
     cmdBuffer = find(PVcmd_vector==1);    
+    
     for i = 1:length(cmdBuffer)
         switch cmdBuffer(i)
             case 1 % GetImage | -gi
                 scriptCmd = sprintf("-SetFileIteration %d",10);
                 modSrv.SendScriptCommands(scriptCmd);
                 modSrv.GetImage(1);
-            case 2 % TSeries | -ts    
-                
+            case 2 % TSeries | -ts                   
+                modSrv.SendScriptCommands("-ts");                
             case 3 % Open Shutter
                 modSrv.SendScriptCommands("-SetHardShutter Open")
             case 4 % Close Shutter
                 modSrv.SendScriptCommands("-SetHardShutter Close")
+                pause(1);
+                
             case 7 % DEBUG
                 scriptCmd = sprintf("-Get");
                 modSrv.SendScriptCommands(scriptCmd);
                 modSrv.SendScriptCommands("-gts");
-
         end
     end
+
+    write(sgSrv,uint8(PVrx))
 
 end
