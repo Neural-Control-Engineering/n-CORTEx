aca08391a399d239d01178fddc5c797c0aa2853d
kelley92 <kelley92@bu.edu>
2024-05-08 18:57:14 -0400

    momentary switch to exp module during build -MS

--- Setup/setSimulinkCodeGenPath.m
+++ Setup/setSimulinkCodeGenPath.m
@@ -1,6 +1,7 @@
 function setSimulinkCodeGenPath(expModulePath)
     cfg = Simulink.fileGenControl('getConfig');
     cfg.CacheFolder = fullfile(expModulePath);
+    % cfg.CacheFolder = '';
     cfg.CodeGenFolder = fullfile(expModulePath);
     Simulink.fileGenControl('setConfig', 'config', cfg,'createDir',false);
 end
