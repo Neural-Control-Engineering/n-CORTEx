a913b53c276c501028a56c0eb9ea9e936305bdb8
C. L. Slater <50306851+codylslater@users.noreply.github.com>
2024-06-13 12:11:38 -0400

    npxls mapping

--- Visualization/Npxls/findRegion.m
+++ Visualization/Npxls/findRegion.m
@@ -1,5 +1,9 @@
 function reg = findRegion(probeRegs, y)
     yRegStart = probeRegs.probe_tip_distance(:,2);
+<<<<<<< Updated upstream
+=======
+    % yRegStart = probeRegs.probe_depth(:,2);
+>>>>>>> Stashed changes
     ydif = yRegStart - y/1000;    
     ydif_neg = ydif(ydif<0);
     ydif_neg_min = max((ydif_neg));
