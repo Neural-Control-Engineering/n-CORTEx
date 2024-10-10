2ed72b70e09b4d26fcc5caa92a99dbdbc0b166c8
ms2565 <mas2565@columbia.edu>
2024-10-10 19:43:26 -0400

    plotHorizon fror vonfrey PSD

---
+++ Visualization/plotHorizon.m
@@ -0,0 +1,16 @@
+function plotHorizon(Ax, f, dfSet, color, legItem)
+    % color in HSV format
+    cDim = color;
+    cDim(2) = 50;
+    cDim(3) = 70;
+    % fh = figure;
+    for i = 1:size(dfSet,1)
+        df = dfSet{i};
+        plot(Ax, f, df,"Color",hsv2rgb(cDim./[360,100,100]))
+        hold on
+    end
+    catDfs = cat(2, dfSet{:});
+    meanDfs = mean(catDfs,2);
+    plot(Ax, f, meanDfs,"Color",hsv2rgb(color./[360,100,100]),"LineWidth",2)
+    % Ax.Legend = [Ax.Legend,legItem];
+end
