c3b2de336c7d05896a776045ffa39174b1c7b128
Marc Sorrentino <mas2565@columbia.edu>
2025-04-06 15:46:51 -0400

    cleanup

--- utils/Nexus/RealtimeControl/cortexCtrl/ctrlKey.m
+++ utils/Nexus/RealtimeControl/cortexCtrl/ctrlKey.m
@@ -39,39 +39,3 @@ classdef ctrlKey < Simulink.IntEnumType
     end
 
 end
-
-% classdef CommandEnum < Simulink.IntEnumType
-%     enumeration
-%         startCapture_npxls (1)
-%         stopCapture_npxls  (2)
-%         resetDevice        (3)
-%     end
-% 
-%     methods (Static)
-%         function str = getStringFromCode(code)
-%             switch code
-%                 case CommandEnum.startCapture_npxls
-%                     str = "startCapture_npxls";
-%                 case CommandEnum.stopCapture_npxls
-%                     str = "stopCapture_npxls";
-%                 case CommandEnum.resetDevice
-%                     str = "resetDevice";
-%                 otherwise
-%                     str = "UNKNOWN";
-%             end
-%         end
-% 
-%         function code = getCodeFromString(str)
-%             switch str
-%                 case "startCapture_npxls"
-%                     code = CommandEnum.startCapture_npxls;
-%                 case "stopCapture_npxls"
-%                     code = CommandEnum.stopCapture_npxls;
-%                 case "resetDevice"
-%                     code = CommandEnum.resetDevice;
-%                 otherwise
-%                     code = CommandEnum(-1); % Or handle error differently
-%             end
-%         end
-%     end
-% end
