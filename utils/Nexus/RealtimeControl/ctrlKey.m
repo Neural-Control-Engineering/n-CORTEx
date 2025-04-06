9a136af8cef9d513ff57549b0ff645bcdb36f11d
Marc Sorrentino <mas2565@columbia.edu>
2025-04-06 14:22:33 -0400

    control and target enumerated keys

--- utils/Nexus/RealtimeControl/ctrlKey.m
+++ utils/Nexus/RealtimeControl/ctrlKey.m
@@ -1,7 +1,77 @@
 classdef ctrlKey < Simulink.IntEnumType
-    enumeration
-        startDataStream_npxls (1)
-        startDataStream_photon (2)
-        endOfTrial (3)
+    enumeration        
+        startDataStream (1)
+        stopDataStream (2)
+        startCapture (3)
+        stopCapture (4)
+        endOfTrial (5)
     end
+
+    methods (Static)
+
+        function code = getCode(cmd)
+            switch cmd
+                case "startDataStream"
+                    code = ctrlKey.startDataStream;
+                case "stopDataStream"
+                    code = ctrlKey.stopDataStream;
+                case "startCapture"
+                    code = ctrlKey.startCapture;
+                case "stopCapture"
+                    code = ctrlKey.stopCapture;
+            end
+        end
+
+        function cmd = getCmd(code)
+            switch code                
+                case ctrlKey.startDataStream
+                    cmd = "startDataStream";
+                case ctrlKey.stopDataStream
+                    cmd = "stopDataStream";
+                case ctrlKey.startCapture
+                    cmd = "startCapture";
+                case ctrlKey.stopCapture
+                    cmd = "stopCapture";
+            end
+        end
+
+        
+    end
+
 end
+
+% classdef CommandEnum < Simulink.IntEnumType
+%     enumeration
+%         startCapture_npxls (1)
+%         stopCapture_npxls  (2)
+%         resetDevice        (3)
+%     end
+% 
+%     methods (Static)
+%         function str = getStringFromCode(code)
+%             switch code
+%                 case CommandEnum.startCapture_npxls
+%                     str = "startCapture_npxls";
+%                 case CommandEnum.stopCapture_npxls
+%                     str = "stopCapture_npxls";
+%                 case CommandEnum.resetDevice
+%                     str = "resetDevice";
+%                 otherwise
+%                     str = "UNKNOWN";
+%             end
+%         end
+% 
+%         function code = getCodeFromString(str)
+%             switch str
+%                 case "startCapture_npxls"
+%                     code = CommandEnum.startCapture_npxls;
+%                 case "stopCapture_npxls"
+%                     code = CommandEnum.stopCapture_npxls;
+%                 case "resetDevice"
+%                     code = CommandEnum.resetDevice;
+%                 otherwise
+%                     code = CommandEnum(-1); % Or handle error differently
+%             end
+%         end
+%     end
+% end
---
+++ utils/Nexus/RealtimeControl/targetKey.m
@@ -0,0 +1,37 @@
+classdef targetKey < Simulink.IntEnumType
+    enumeration
+        npxls  (1)
+        photon (2)
+        photom (3)
+    end
+
+    methods (Static)
+
+        function code = getCode(cmd)
+            switch cmd
+                case "npxls"
+                    code = targetKey.npxls;
+                case "photon"
+                    code = targetKey.photon;
+                case "photom"
+                    code = targetKey.photom;
+                otherwise
+                    code = targetKey(-1); % Or handle error as needed
+            end
+        end
+
+        function cmd = getCmd(code)
+            switch code
+                case targetKey.npxls
+                    cmd = "npxls";
+                case targetKey.photon
+                    cmd = "photon";
+                case targetKey.photom
+                    cmd = "photom";
+                otherwise
+                    cmd = "UNKNOWN"; % Or use "" or raise error
+            end
+        end
+
+    end
+end
