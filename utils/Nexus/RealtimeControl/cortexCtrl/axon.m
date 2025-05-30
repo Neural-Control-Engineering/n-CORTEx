971792476dbc1622c0e8894b4c6cfcf719384abc
NEC Lab Shared Resources <50306851+codyslater@users.noreply.github.com>
2025-05-30 08:44:33 -0400

    'axon' transmission type implemented as bus - MS

--- utils/Nexus/RealtimeControl/cortexCtrl/axon.m
+++ utils/Nexus/RealtimeControl/cortexCtrl/axon.m
@@ -1,19 +1,47 @@
 function ax = axon(class)
+    numCmds = numel(enumeration('ctrlKey'));
+    bufferSize = 1000;
+    maxDim = 3;
     switch class
         case "command"
+            ax = Simulink.Bus;
+            %% CMD
             elems(1) = Simulink.BusElement;
-            elems(1).Name = 'CMD'
-            elems(1).DataType = 'uint'
-            ax.CMD
-            ax.SZE
+            elems(1).Name = 'CMD';
+            elems(1).DataType = 'uint';
+            elems(1).Dimensions = [numCmds,1];
+            %% SZE
+            elems(2) = Simulink.BusElement;
+            elems(2).Name = 'SZE';
+            elems(2).DataType = 'uint';
+            elems(2).Dimensions = [numCmds, maxDim];
+            %% PYD
+            elems(3) = Simulink.BusElement;
+            elems(3).Name = "PYD";
+            elems(3).DataType = 'uint';
+            elems(3).Dimensions = [numCmds, bufferSize];
+            %% assembly
+            ax.Elements = elems;           
+
         case "stream"            
-            ax.SZE
-            ax.PYD
+            ax = Simulink.Bus;            
+            %% SZE
+            elems(1) = Simulink.BusElement;
+            elems(1).Name = 'SZE';
+            elems(1).DataType = 'uint';
+            elems(1).Dimensions = [numCmds, maxDim];
+            %% PYD
+            elems(2) = Simulink.BusElement;
+            elems(2).Name = "PYD";
+            elems(2).DataType = 'uint';
+            elems(2).Dimensions = [numCmds, bufferSize];
+            %% assembly
+            ax.Elements = elems;           
     end
 end
 
-ax.CMD
-ax.SZE
-ax.PYD.signal_L
-ax.PYD.signal_M
-ax.PYD.tag
+% ax.CMD
+% ax.SZE
+% ax.PYD.signal_L
+% ax.PYD.signal_M
+% ax.PYD.tag
