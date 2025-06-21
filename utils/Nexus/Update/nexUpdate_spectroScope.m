c00b637c4509363171930fad7497426335bc84ec
ms2565 <mas2565@columbia.edu>
2025-06-21 12:59:50 -0400

    nexus with specparams

---
+++ utils/Nexus/Update/nexUpdate_spectroScope.m
@@ -0,0 +1,5 @@
+function nexUpdate_spectroScope(nexObj)
+    % retrieve precomputed spectral parameters (if available)
+    % otherwise generate a new DF_specs
+    % visualize update
+end
--- utils/Nexus/Update/nex_updateChildren.m
+++ utils/Nexus/Update/nex_updateChildren.m
@@ -1,13 +1,15 @@
 function nex_updateChildren(nexon, nexObj)
-    nexObjChildrenList = fieldnames(nexObj.Children);
-    for i = 1:length(nexObjChildrenList)
-        nexObjChildName = nexObjChildrenList{i};
-        nexObjChild = nexObj.Children.(nexObjChildName);
-        % run update method
-        try
-            nexObjChild.updateScope(nexon);
-        catch e
-            disp(getReport(e));
+    if ~isempty(nexObj.Children)
+        nexObjChildrenList = fieldnames(nexObj.Children);
+        for i = 1:length(nexObjChildrenList)
+            nexObjChildName = nexObjChildrenList{i};
+            nexObjChild = nexObj.Children.(nexObjChildName);
+            % run update method
+            try
+                nexObjChild.updateScope(nexon);
+            catch e
+                disp(getReport(e));
+            end
         end
     end
 end
