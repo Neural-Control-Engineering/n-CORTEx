97461a77d8e66d12f54f09472ab49a5bd26cd920
Marc Sorrentino <mas2565@columbia.edu>
2026-07-22 14:32:32 -0400

    Cache Repair Reboot

---
+++ Extraction/Repair/repairCache.m
@@ -0,0 +1,6 @@
+function repairCache(nCORTEx)
+    % Search Local directory for stray sessions
+    % cross reference with RAW extraction log and existing session cache to index un-cached sessions
+    % retrieve metaparameters
+    % surgical replace into cache and resave
+end
